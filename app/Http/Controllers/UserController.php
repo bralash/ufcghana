1a9d5f66fde07b67b0a5dc4550b8c4fcb870a021
Emmanuel Asaber <emmanuelasaber@gmail.com>
2018-04-09 21:01:32 -0700

    SMS Test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -67,30 +67,55 @@ class UserController extends Controller
         return redirect('/auth/login');
     }
 
-    public function sms() {
-//        $url = "https://api.hubtel.com/v3/messages/send?"
-//            . "From=Unity"
-//            . "&To=%2B233503123939"
-//            . "&Content=Hello%2C+world"
-//            . "&ClientId=frbiodfp"
-//            . "&ClientSecret=tkreglmn"
-//            . "&RegisteredDelivery=true";
-//        // Fire the request and wait for the response
-//        $response = file_get_contents($url) ;
-//        var_dump($response);
+//    public function sms() {
+////        $url = "https://api.hubtel.com/v3/messages/send?"
+////            . "From=Unity"
+////            . "&To=%2B233503123939"
+////            . "&Content=Hello%2C+world"
+////            . "&ClientId=frbiodfp"
+////            . "&ClientSecret=tkreglmn"
+////            . "&RegisteredDelivery=true";
+////        // Fire the request and wait for the response
+////        $response = file_get_contents($url) ;
+////        var_dump($response);
+//
+//        $client = new Client();
+//        $response = $client->get('https://api.hubtel.com/v3/messages/send',[
+//            'form_params' => [
+//                'From' => 'Unity',
+//                'To' => '%2B233503123939',
+//                'Content' => 'Hello world',
+//                'ClientId' => 'cxdvnlbi',
+//                'ClientSecret' => 'vbjytixl',
+//                'RegisteredDelivery' => true
+//            ],
+//        ]);
+//        $response = json_decode($response->getBody(), true);
+//
+//    }
 
-        $client = new Client();
-        $response = $client->get('https://api.hubtel.com/v3/messages/send',[
-            'form_params' => [
-                'From' => 'Unity',
-                'To' => '%2B233503123939',
-                'Content' => 'Hello world',
-                'ClientId' => 'frbiodfp',
-                'ClientSecret' => 'tkreglmn',
-                'RegisteredDelivery' => true
-            ],
-        ]);
-        $response = json_decode($response->getBody(), true);
+    public function sms()
+    {
+        $message = rawurlencode('Hi Emmanuel this is a test');
 
+        $from = 'UFC Ghana';
+        $to = '+233503123939';
+
+        $curl = curl_init();
+        $clientId = 'cxdvnlbi';
+        $clientSecret = 'vbjytixl';
+        $url = "https://api.hubtel.com/v1/messages/send?From=$from&To=$to&Content=$message&ClientId=cxdvnlbi&ClientSecret=vbjytixl&RegisteredDelivery=true";
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+        ));
+        curl_exec($curl);
+        curl_close($curl);
     }
+
 }
